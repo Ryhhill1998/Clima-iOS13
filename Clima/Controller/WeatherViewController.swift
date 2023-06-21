fa80531b315e609f76e8980b256188161766543a
ryhhill1998 <ryhhill1998@outlook.com>
2023-06-21 09:54:58 +0100

    Added switch statement to get condition name from api

--- Clima/Controller/WeatherViewController.swift
+++ Clima/Controller/WeatherViewController.swift
@@ -15,6 +15,8 @@ class WeatherViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet weak var cityLabel: UILabel!
     @IBOutlet weak var searchTextField: UITextField!
     
+    var weatherManager = WeatherManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -40,6 +42,8 @@ class WeatherViewController: UIViewController, UITextFieldDelegate {
     }
     
     func textFieldDidEndEditing(_ textField: UITextField) {
+        guard let cityQuery = searchTextField.text else { return }
+        weatherManager.fetchWeatherData(for: cityQuery)
         searchTextField.text = ""
     }
 }
--- Clima/Model/WeatherManager.swift
+++ Clima/Model/WeatherManager.swift
@@ -13,5 +13,67 @@ struct WeatherManager {
     
     func fetchWeatherData(for cityName: String) {
         let urlString = weatherURL + cityName
+        print(urlString)
+        performRequest(to: urlString)
+    }
+    
+    func performRequest(to urlString: String) {
+        // 1. Create a URL object
+        guard let url = URL(string: urlString) else { return }
+        
+        // 2. Create a URL session
+        let session = URLSession(configuration: .default)
+        
+        // 3. Give the session a task
+        let task = session.dataTask(with: url) {(data, response, error) in
+            if error != nil {
+                print(error!)
+                return
+            }
+            
+            guard let safeData = data else { return }
+            parseJSON(weatherData: safeData)
+        }
+        
+        // 4. Start the task
+        task.resume()
+    }
+    
+    func parseJSON(weatherData: Data) {
+        let decoder = JSONDecoder()
+        do {
+            let decodedData = try decoder.decode(WeatherData.self, from: weatherData)
+            let id = decodedData.weather[0].id
+            let conditionName = getConditionName(weatherCode: id)
+            print(id)
+            print(conditionName)
+        } catch {
+            print(error)
+        }
+    }
+    
+    func getConditionName(weatherCode: Int) -> String {
+        var conditionName: String
+        
+        switch weatherCode {
+        case 200...232:
+            conditionName = "Thunderstorm"
+        case 300...321:
+            conditionName = "Drizzle"
+        case 500...531:
+            conditionName = "Rain"
+        case 600...622:
+            conditionName = "Snow"
+        case 701...781:
+            conditionName = "Atmosphere"
+        case 800:
+            conditionName = "Clear"
+        case 801...804:
+            conditionName = "Clouds"
+        default:
+            conditionName = "Invalid code"
+        }
+        
+        return conditionName
     }
 }
